94fec12fcf53980490d5bcbe7115baf544dc14cc
lostjarred <lostotaku@outlook.com>
2021-02-26 13:03:07 +1100

    added items

--- src/main/java/com/lo93/foodexpansion/core/init/items/ModItems.java
+++ src/main/java/com/lo93/foodexpansion/core/init/items/ModItems.java
@@ -7,5 +7,7 @@ import net.minecraft.creativetab.CreativeTabs;
 public class ModItems {
 	public static final ItemBasic TOOLMORTARPESTAL = (ItemBasic) new ItemBasic("toolmortarpestal").setCreativeTab(CreativeTabs.FOOD);
 	public static final ItemBasic TOOLKNIFE = (ItemBasic) new ItemBasic("toolknife").setCreativeTab(CreativeTabs.FOOD);
-	public static final ItemBasic WHOLEFLOUR = (ItemBasic) new ItemBasic("wholeflour").setCreativeTab(CreativeTabs.FOOD);
+	public static final ItemBasic ITEMWHOLEFLOUR = (ItemBasic) new ItemBasic("itemwholeflour").setCreativeTab(CreativeTabs.FOOD);
+	public static final ItemBasic ITEMWHEATGRAIN = (ItemBasic) new ItemBasic("itemwheatgrain").setCreativeTab(CreativeTabs.FOOD);
+	public static final ItemBasic TOOLBAKINGTRAY = (ItemBasic) new ItemBasic("toolbakingtray").setCreativeTab(CreativeTabs.FOOD);
 }
